c069fcc3541fcaae1d380a00613ae462a06712d8
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2024-11-18 17:49:06 +0100

    The phone book step 1 implemented

--- solutions/part2/courseinfo/src/App.jsx
+++ solutions/part2/courseinfo/src/App.jsx
@@ -46,7 +46,7 @@ const App = () => {
     },
     {
       name: "Databases",
-      id: 2,
+      id: 4,
       parts: [
         {
           name: "MySQL",
---
+++ solutions/part2/phonebook/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName('')
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form  onSubmit={addName}>
+        <div>
+          name:{" "}
+          <input
+            onChange={(event) => setNewName(event.target.value)}
+            value={newName}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+        <div>debug: {newName}</div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </div>
+  );
+};
+
+export default App;
