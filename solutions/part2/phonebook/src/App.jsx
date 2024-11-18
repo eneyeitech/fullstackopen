fee901a6d995d76fa63d38df1123199d6b3fd60b
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2024-11-18 18:16:08 +0100

    The phone book step 3 implemented

--- solutions/part2/phonebook/src/App.jsx
+++ solutions/part2/phonebook/src/App.jsx
@@ -1,25 +1,35 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "08051185104" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
-    const found = persons.find((person)=> person.name === newName);
-    console.log('index', found)
-    if(found){
+    const nameFound = persons.find((person) => person.name === newName);
+    const numberFound = persons.find((person) => person.number === newNumber);
+    console.log("index", nameFound, numberFound);
+    if (nameFound) {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    setPersons(persons.concat({ name: newName }));
-    setNewName('')
+    if (numberFound) {
+      alert(`${newNumber} is already added to phonebook`);
+      return;
+    }
+    if(!(newName && newNumber)){
+      alert(`All fields must be field`);
+      return;
+    }
+    setPersons(persons.concat({ name: newName, number: newNumber }));
+    setNewName("");
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form  onSubmit={addName}>
+      <form onSubmit={addName}>
         <div>
           name:{" "}
           <input
@@ -28,12 +38,21 @@ const App = () => {
           />
         </div>
         <div>
+          number:{" "}
+          <input
+            onChange={(event) => setNewNumber(event.target.value)}
+            value={newNumber}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
         <div>debug: {newName}</div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map((person) => (
+        <p key={person.name}>{person.name} {person.number}</p>
+      ))}
     </div>
   );
 };
